25d396bf82305091e883b3cc7e6e06a00c9e9d6d
iqmalakur <iakbarkurnia@gmail.com>
2024-09-22 20:53:47 +0700

    update position field

---
+++ prisma/migrations/20240922135311_update_position/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE `Employee` (
+    `nik` VARCHAR(20) NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `area` VARCHAR(50) NOT NULL,
+    `role` VARCHAR(50) NOT NULL,
+    `position` ENUM('OnSite', 'Koordinator') NOT NULL DEFAULT 'OnSite',
+    `profile_photo` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`nik`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ApiKey` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `key` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
