31766293cf053717d19bcde9b4c76f6c261f47ea
iqmalakur <iakbarkurnia@gmail.com>
2024-09-26 13:42:30 +0700

    restore prisma migration

--- prisma/migrations/20240926064056_init/migration.sql
+++ prisma/migrations/20240926064056_init/migration.sql
@@ -10,3 +10,11 @@ CREATE TABLE `Employee` (
 
     PRIMARY KEY (`nik`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ApiKey` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `key` VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
