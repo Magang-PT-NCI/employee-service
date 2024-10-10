9672aae073a2aaeb34dac08e75b4ee9daee71bc9
iqmalakur <iakbarkurnia@gmail.com>
2024-10-10 17:35:14 +0700

    add winston daily rotate file

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -1,13 +1,4 @@
 import { zeroPadding } from './common.utils';
-import { IS_SHOW_DATE } from '../config/logger.config';
-
-const getDateString = (date: Date) => {
-  const year: string = zeroPadding(date.getFullYear(), 4);
-  const month: string = zeroPadding(date.getMonth() + 1, 2);
-  const day: string = zeroPadding(date.getDate(), 2);
-
-  return `${year}-${month}-${day}`;
-};
 
 const getTimeString = (date: Date) => {
   const hours: string = zeroPadding(date.getHours());
@@ -19,7 +10,5 @@ const getTimeString = (date: Date) => {
 };
 
 export const getDateFormat = (date: Date) => {
-  return IS_SHOW_DATE
-    ? `${getDateString(date)} ${getTimeString(date)}`
-    : getTimeString(date);
+  return getTimeString(date);
 };
